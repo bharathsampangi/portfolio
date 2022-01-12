c366c13d8eb4bff186cb743009cadfc1a97ea1e7
Bharath S <36877669+bharathsampangi@users.noreply.github.com>
2022-01-12 08:45:28 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import '../styles/Header.scss'
+import '../variables/Typography.scss'
 
 function Header() {
     return (
         <>
             <div className="header">
                 <div className="header__content">
-                    Header
+                    <div class="header__content-text">
+                        <p>Hello</p>
+                        <h1>I'm Bharath Sampangi</h1>
+                        <h3>Fullstack Dev</h3>
+                        <button class="btn">More About Me</button>
+                    </div>
                 </div>
                 <div className="header__img">
-                    <img src="images/profile.png" alt="" />
+                    <div className="header__img-outline">
+                        <div className="header__img-inline">
+                            <img src="images/profile2.png" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
